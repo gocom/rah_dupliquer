cded9036820a9f6e7e5520c47cf1ce725c8e1937
Jukka Svahn <web@rahinaa.biz>
2012-11-04 04:44:59 +0200

    Initial commit.

---
+++ rah_dupliquer.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * rah_dupliquer plugin for Textpattern CMS.
+ *
+ * @author  Jukka Svahn
+ * @date    2012-
+ * @license GNU GPLv2
+ * @link    https://github.com/gocom/rah_dupliquer
+ * 
+ * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Licensed under GNU Genral Public License version 2
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+	new rah_dupliquer();
+
+class rah_dupliquer
+{
+	/**
+	 * Constructor.
+	 */
+
+	public function __construct()
+	{
+		global $event;
+
+		if ($event === 'article')
+		{
+			register_callback(array($this, 'styles'), 'admin_side', 'head_end');
+			register_callback(array($this, 'javascript'), 'admin_side', 'head_end');
+		}
+	}
+
+	/**
+	 * Styles.
+	 */
+
+	public function styles()
+	{
+		echo <<<EOF
+			<style type="text/css">
+				.rah_dupliquer_tip
+				{
+					margin: 1em 0 0 0.5em;
+					display: none;
+					vertical-align: baseline;
+				}
+			</style>
+EOF;
+	}
+
+	/**
+	 * Initializes the JavaScript.
+	 */
+
+	public function javascript()
+	{
+		$js = <<<EOF
+			(function() {
+				$(document).ready(function() {
+					var tipText = 'CTRL+D';
+
+					if (navigator.userAgent.indexOf('Mac OS X') !== -1)
+					{
+						tipText = '&#8984;+D';
+					}
+
+					$('form .publish').eq(0)
+						.after('<small class="rah_dupliquer_tip information">'+tipText+'</small>')
+						.hover(function() {
+							$(this).siblings('.rah_dupliquer_tip').fadeToggle();
+						});
+				});
+
+				$(window).keydown(function(e) {
+					if (e.which === 68 || (String.fromCharCode(e.which).toLowerCase() === 'd' && (e.metaKey || e.ctrlKey)))
+					{
+						var obj = $('form .publish');
+						if (obj.length)
+						{
+							e.preventDefault();
+							var editor = obj.eq(0).parents('form');
+							var form = editor.clone();
+							form.find('[name=exp_year], [name=url_title]').val('');
+							form.find('#title').val(form.find('#title').val());
+							form.append(
+								'<input type="hidden" name="publish" value="1" />'+
+								'<input type="hidden" name="publish_now" value="1" />'
+							);
+							form.removeClass('async');
+							editor.replaceWith(form);
+							form.submit();
+						}
+					}
+				});
+			})();
+EOF;
+
+		echo script_js($js);
+	}
+}
+
+?>
