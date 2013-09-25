ca77f1362c18ef28a37c571cba7da6a031e7615a
Jukka Svahn <web@rahinaa.biz>
2013-09-25 13:44:13 +0300

    Indent with spaces.

--- rah_dupliquer.php
+++ rah_dupliquer.php
@@ -13,7 +13,7 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	new rah_dupliquer();
+    new rah_dupliquer();
 
 /**
  * The plugin class.
@@ -21,110 +21,110 @@
 
 class rah_dupliquer
 {
-	/**
-	 * Constructor.
-	 */
+    /**
+     * Constructor.
+     */
 
-	public function __construct()
-	{
-		register_callback(array($this, 'styles'), 'admin_side', 'head_end');
-		register_callback(array($this, 'javascript'), 'admin_side', 'head_end');
-	}
+    public function __construct()
+    {
+        register_callback(array($this, 'styles'), 'admin_side', 'head_end');
+        register_callback(array($this, 'javascript'), 'admin_side', 'head_end');
+    }
 
-	/**
-	 * Styles.
-	 */
+    /**
+     * Styles.
+     */
 
-	public function styles()
-	{
-		echo <<<EOF
-			<style>
-				.rah_dupliquer_tip
-				{
-					visibility: hidden;
-				}
-			</style>
+    public function styles()
+    {
+        echo <<<EOF
+            <style>
+                .rah_dupliquer_tip
+                {
+                    visibility: hidden;
+                }
+            </style>
 EOF;
-	}
+    }
 
-	/**
-	 * Initializes the JavaScript.
-	 */
+    /**
+     * Initializes the JavaScript.
+     */
 
-	public function javascript()
-	{
-		$js = <<<EOF
-			textpattern.Route.add('article, css, page, form', function ()
-			{
-				if (!$('#txp_clone').length && $('[name=publish]').length)
-				{
-					return;
-				}
+    public function javascript()
+    {
+        $js = <<<EOF
+            textpattern.Route.add('article, css, page, form', function ()
+            {
+                if (!$('#txp_clone').length && $('[name=publish]').length)
+                {
+                    return;
+                }
 
-				var tipText = 'CTRL+D';
+                var tipText = 'CTRL+D';
 
-				if (navigator.userAgent.indexOf('Mac OS X') !== -1)
-				{
-					tipText = '&#8984;+D';
-				}
+                if (navigator.userAgent.indexOf('Mac OS X') !== -1)
+                {
+                    tipText = '&#8984;+D';
+                }
 
-				$('form .publish').eq(0)
-					.after(' <small class="rah_dupliquer_tip information">'+tipText+'</small> ')
-					.hover(
-						function()
-						{
-							$(this).siblings('.rah_dupliquer_tip')
-								.css('opacity', 0)
-								.css('visibility', 'visible')
-								.fadeTo(600, 1);
-						},
-						function()
-						{
-							$(this).siblings('.rah_dupliquer_tip')
-								.fadeTo(300, 0, function() {
-									$(this).css('visibility', 'hidden');
-								});
-						}
-					)
-					.click(function() {
-						$(this).siblings('.rah_dupliquer_tip')
-							.css('opacity', 0)
-							.css('visibility', 'hidden');
-					});
+                $('form .publish').eq(0)
+                    .after(' <small class="rah_dupliquer_tip information">'+tipText+'</small> ')
+                    .hover(
+                        function()
+                        {
+                            $(this).siblings('.rah_dupliquer_tip')
+                                .css('opacity', 0)
+                                .css('visibility', 'visible')
+                                .fadeTo(600, 1);
+                        },
+                        function()
+                        {
+                            $(this).siblings('.rah_dupliquer_tip')
+                                .fadeTo(300, 0, function() {
+                                    $(this).css('visibility', 'hidden');
+                                });
+                        }
+                    )
+                    .click(function() {
+                        $(this).siblings('.rah_dupliquer_tip')
+                            .css('opacity', 0)
+                            .css('visibility', 'hidden');
+                    });
 
-				$(window).keydown(function (e)
-				{
-					if (String.fromCharCode(e.which).toLowerCase() === 'd' && (e.metaKey || e.ctrlKey))
-					{
-						var obj = $('.rah_dupliquer_tip');
+                $(window).keydown(function (e)
+                {
+                    if (String.fromCharCode(e.which).toLowerCase() === 'd' && (e.metaKey || e.ctrlKey))
+                    {
+                        var obj = $('.rah_dupliquer_tip');
 
-						if (obj.length)
-						{
-							e.preventDefault();
-							var clone = $('#txp_clone');
+                        if (obj.length)
+                        {
+                            e.preventDefault();
+                            var clone = $('#txp_clone');
 
-							if (clone.length)
-							{
-								clone.click();
-								return;
-							}
+                            if (clone.length)
+                            {
+                                clone.click();
+                                return;
+                            }
 
-							var form = obj.eq(0).parents('form');
-							form.find('[name=exp_year], [name=url_title]').val('');
-							form.append(
-								'<input type="hidden" name="publish" value="1" />'+
-								'<input type="hidden" name="publish_now" value="1" />'+
-								'<input type="hidden" name="Status" value="1" />'
-							);
-							form.off('submit.txpAsyncForm').submit();
-						}
-					}
-				});
-			});
+                            var form = obj.eq(0).parents('form');
+                            form.find('[name=exp_year], [name=url_title]').val('');
+                            form.append(
+                                '<input type="hidden" name="publish" value="1" />'+
+                                '<input type="hidden" name="publish_now" value="1" />'+
+                                '<input type="hidden" name="Status" value="1" />'
+                            );
+                            form.off('submit.txpAsyncForm').submit();
+                        }
+                    }
+                });
+            });
 EOF;
 
-		echo script_js($js);
-	}
+        echo script_js($js);
+    }
 }
 
 ?>
